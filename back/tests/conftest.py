8db234e6b3d331d5e05b11f25726e65222427c18
bartibarti <bartibarti@BartiBarti.>
2026-05-20 17:33:07 +0200

    Added integration tests for user_service.add_user. Replaced real database with SQLite

--- back/tests/conftest.py
+++ back/tests/conftest.py
@@ -1,14 +1,19 @@
 import pytest
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
+from back.structure import User
+from back.database import Base
+from back.service.user_service import add_user
 
-DB_URL = "postgresql://admin:password123@localhost:5432/finance_app"
+DB_URL = "sqlite:///:memory:"
 
-engine = sqlalchemy.create_engine(DB_URL)
+engine = sqlalchemy.create_engine(DB_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(bind=engine)
 
 @pytest.fixture
 def db_session():
+    Base.metadata.create_all(bind=engine)
+    
     connection = engine.connect()
     transaction = connection.begin()
 
--- back/tests/test_user_service.py
+++ back/tests/test_user_service.py
@@ -15,3 +15,34 @@ def test_create_user():
     assert new_user.email == "alice@gmail.com"
     assert new_user.name == "alice"
 
+
+def test_add_user_success(db_session):
+    user_data = UserCreate(
+        email="alice@gmail.com",
+        password="1234",
+        name="alice"
+    )
+
+    new_user = user_service.create_user(user_data)
+    result = user_service.add_user(db_session, new_user)
+
+    assert result.name == "alice"
+    assert result.email == "alice@gmail.com"
+
+
+def test_add_user_duplicate(db_session):
+    user_data = UserCreate(
+        email="alice@gmail.com",
+        password="1234",
+        name="alice"
+    )
+
+    user1 = user_service.create_user(user_data)
+    user2 = user_service.create_user(user_data)
+
+    user_service.add_user(db_session, user1)
+    
+    with pytest.raises(Exception):
+        user_service.add_user(db_session, user2)
+
+

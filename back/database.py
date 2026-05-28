d0c72e704dbf9037900df87520970666d13b48e8
Toporio <grzesiotoporski@gmail.com>
2026-05-28 19:44:26 +0200

    added db hosting

--- back/database.py
+++ back/database.py
@@ -1,8 +1,17 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-DB_URL = "postgresql://admin:password123@localhost:5432/finance_app"
+load_dotenv()
+DB_URL = os.getenv("DATABASE_URL")
+if not DB_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
 
+if DB_URL.startswith("postgresql://"):
+    DB_URL = DB_URL.replace("postgres://", "postgresql://", 1)
 # Engine manages connections to the database, SessionLocal is a factory for creating new sessions, Base is the base class for our models
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- back/main.py
+++ back/main.py
@@ -1,8 +1,10 @@
-import back.database
-import back.structure
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+import back.database
+import back.structure
 from back.routers import user_router
 
 # ! Important !
@@ -11,6 +13,7 @@ from back.routers import user_router
 # Don't remove this comment until final version or docs are disabled
 # Don't disable too early tho super good for testing
 # ! Important !
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):

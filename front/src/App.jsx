b5b997aa39fe7d3d593743b5158deebd38f2c821
lukasz <lukkrz04@gmail.com>
2026-07-05 21:47:28 +0200

    changed Toast position to top-center to be more friendly to mobile devices

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -395,7 +395,7 @@ function App() {
           </Routes>
         </Suspense>
       </BrowserRouter>
-      <Toaster position="bottom-right" richColors />
+      <Toaster position="top-center" richColors pauseOnHover />
     </TooltipProvider>
   );
 }

b23836e354dfc73923c2254b4e17912bff1b7fb1
lukasz <lukkrz04@gmail.com>
2026-07-05 12:30:51 +0200

    repaired google login by adding new origin in main.py

--- back/main.py
+++ back/main.py
@@ -39,12 +39,13 @@ origins = [
     "http://localhost:5173",
     "http://localhost:5174",
     "http://127.0.0.1:3000",
+    "https://finance-app-lukaszkrzem.vercel.app",
 ]
 
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
